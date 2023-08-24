dc57712a2f77a0ec72f81d54dc8497ddbcc7003b
Patiphon Khamsam <Patiphon.k40@gmail.com>
2023-08-24 13:02:49 +0700

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,76 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
+
+/*===== MENU SHOW =====*/
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    });
+}
+
+/*===== MENU HIDDEN =====*/
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
 
 /*=============== ADD BLUR TO HEADER ===============*/
-
+const blurHeader = () => {
+    const header = document.getElementById('header')
+    // When the scroll ois greater than 50 viewport height, add the blur-header class  
+    this.scrollY >= 50? header.classList.add('blur-header')
+                      : header.classList.remove('blur-header');
+}
+window.addEventListener('scroll',blurHeader);
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
-
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 3000,
+    delay: 400,
+    // reset: true
+})
+
+sr.reveal(`.home__data, .explore__data, .explore__user, .footer__content`)
+sr.reveal(`.home__card`,{delay: 600, distance: '100px', interval:100})
+sr.reveal(`.about__data, .join__image`,{origin: 'right'})
+sr.reveal(`.about__image, join__data`,{origin: 'left'})
+sr.reveal(`.popular__card`,{interval: 200})
+
+
